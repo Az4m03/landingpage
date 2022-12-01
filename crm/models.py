1161d540ff011d42d5c5ef0b9b6c7d0abeae3c4d
Azam <abdurazakovazam01@gmail.com>
2022-12-01 20:25:42 +0500

    Carusel, Price, CRM

--- crm/models.py
+++ crm/models.py
@@ -1,10 +1,39 @@
 from django.db import models
 
 
+class StatusCrm(models.Model):
+    status_name = models.CharField(max_length=200, verbose_name='Status name')
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        verbose_name = 'Status'
+        verbose_name_plural = 'Statuses'
+
+
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=200)
     order_phone = models.CharField(max_length=200)
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Status')
 
     def __str__(self):
         return self.order_name
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+
+class ComentCrm(models.Model):
+    coment_binding = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Bid')
+    coment_text = models.TextField(verbose_name='Commentary text')
+    coment_dt = models.DateTimeField(auto_now=True, verbose_name='Date of creating')
+
+    def __str__(self):
+        return self.coment_text
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
--- landingpage/settings.py
+++ landingpage/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'price.apps.PriceConfig',
+    'cms.apps.CmsConfig',
+    'crm.apps.CrmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crm.apps.CrmConfig',
 ]
 
 MIDDLEWARE = [
