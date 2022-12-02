b8550bf8862f576a1fba6c83a77788f300b041a6
Azam <abdurazakovazam01@gmail.com>
2022-12-02 23:48:52 +0500

    create telegram bot

--- crm/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.first_page),
-    path('thanks/', views.thanks_page, name='thanks_page')
-]
--- landingpage/urls.py
+++ landingpage/urls.py
@@ -18,7 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from crm import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('crm.urls'))
+    path('', views.first_page),
+    path('thanks/', views.thanks_page, name='thanks_page')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
