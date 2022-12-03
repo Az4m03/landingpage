e500ac9911b400ede089a3e3a0eb60dd769e98ac
Azam <abdurazakovazam01@gmail.com>
2022-12-03 22:13:42 +0500

    Admin setup and create users

--- telebot/sendmessage.py
+++ telebot/sendmessage.py
@@ -1,27 +1,42 @@
 import requests
 from .models import TeleSettings
-import Token
 
-token = Token.TOKEN
-chat_id = Token.CHAT_ID
+token = '5989660093:AAE3aytKP4ABv5_4HtWuaHEF4gXYQAqyuLc'
+chat_id = '-870592460'
 text = 'landingpage_2'
 
 
 def sendTelegram(tg_name, tg_phone):
-    settings = TeleSettings.objects.get(pk=1)
-    token = str(settings.tg_token)
-    chat_id = str(settings.tg_chat)
-    text = str(settings.tg_message)
-    api = 'https://api.telegram.org/bot'
-    method = api + token + '/sendMessage'
+    if TeleSettings.objects.get(pk=1):
+        settings = TeleSettings.objects.get(pk=1)
+        token = str(settings.tg_token)
+        chat_id = str(settings.tg_chat)
+        text = str(settings.tg_message)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendMessage'
 
-    part_1 = text[0:text.find('{')]
-    part_2 = text[text.find('}')+1:text.rfind('{')]
-    part_3 = text[text.rfind('}'):-1]
+        if text.find('{') and text.find('}') and text.rfind('{') and text.rfind('}'):
+            part_1 = text[0:text.find('{')]
+            part_2 = text[text.find('}')+1:text.rfind('{')]
+            part_3 = text[text.rfind('}'):-1]
 
-    text_slise = part_1 + tg_name + part_2 + tg_phone + part_3
+            text_slise = part_1 + tg_name + part_2 + tg_phone + part_3
+        else:
+            text_slise = text
+        try:
+            req = requests.post(method, data={
+                'chat_id': chat_id,
+                'text': text_slise
+            })
+        except:
+            pass
+        finally:
+            if req.status_code != 200:
+                print('Send error')
+            elif req.status_code == 500:
+                print('Error 500')
+            else:
+                print('Every thing ok the message has been sent')
 
-    req = requests.post(method, data={
-        'chat_id': chat_id,
-        'text': text_slise
-    })
+    else:
+        pass
